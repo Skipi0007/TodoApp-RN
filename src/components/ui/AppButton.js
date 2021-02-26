d7c7277991843e0a3ff19fca0e8d8c15ed1cbd37
Skipi <skipi0007@mail.ru>
2021-02-26 16:22:16 +0300

    add new file

---
+++ src/components/ui/AppButton.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {StyleSheet, View, TouchableOpacity} from 'react-native'
+import {AppTextBold} from './AppTextBold'
+
+export const AppButton = ({ children}) => {
+    return (
+        <TouchableOpacity>
+            <View>
+
+            </View>
+        </TouchableOpacity>
+    )
+}
