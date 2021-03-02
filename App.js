f64836e206ebfca04c53e5bb50b017867cc56c3e
Skipi <skipi0007@mail.ru>
2021-03-02 10:37:30 +0300

    app adapted

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import {AppLoading} from 'expo-app-loading'
 import { Navbar } from './src/components/Navbar'
 import { MainScreen } from './src/screens/MainScreen';
 import { TodoScreen } from './src/screens/TodoScreen';
+import { THEME } from './src/themes';
 
 
   Font.loadAsync({
@@ -98,7 +99,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    paddingHorizontal: 30,
+    paddingHorizontal: THEME.PADDING_HORIZONTAL,
     paddingVertical: 20
 
   },
--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import {StyleSheet, View, Button} from 'react-native'
+import {StyleSheet, View, Button, Dimensions} from 'react-native'
 import {FontAwesome, AntDesign} from '@expo/vector-icons'
 import { EditModal } from '../components/EditModal'
 import { AppCard } from '../components/ui/AppCard'
@@ -58,7 +58,7 @@ const styles = StyleSheet.create({
     },
 
     button: {
-        width: '40%'
+        width: Dimensions.get('window').width / 3
     },
 
     title: {
