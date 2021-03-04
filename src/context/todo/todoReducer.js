3adf6505f548464c0d706e74c4d07e8632778b0b
Skipi <skipi0007@mail.ru>
2021-03-04 15:17:03 +0300

    reducer modifyed

--- src/context/todo/todoReducer.js
+++ src/context/todo/todoReducer.js
@@ -1,21 +1,29 @@
 import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from "./types"
 
-export const todoReducer = (state, action) => {
-    switch (action.type) {
-        case ADD_TODO: return {...state, todos: [...state, todo, {
+const handlers = {
+    [ADD_TODO]: (state, {title}) => ({
+        ...state, 
+        todos: [
+            ...state, todo, 
+            {
             id: Date.now().toString(),
-            title: action.title
-            }]}
-        case REMOVE_TODO: return {...state, todos: state.todos.filter(todo => todo.id !== action.id)}
-        case UPDATE_TODO: return {...state, todos: state.todos.map(todo => {
-            if (todo.id === action.id) {
-                todo.title = action.title
+            title
             }
-            return state
-        })}
-        defaultlt: return state
-        
-    }
+        ]
+    }),
+    [REMOVE_TODO]: (state, {id}) => ({...state, todos: state.todos.filter(todo => todo.id !== id)}),
+    [UPDATE_TODO]: (state, {title, id}) => ({...state, todos: state.todos.map(todo => {
+        if (todo.id === id) {
+            todo.title = title
+        }
+        return state})}),
+        DEFAULT: state => state
+}
+
+export const todoReducer = (state, action) => {
+    const handler = handlers[action.type] || handlers.DEFAULT
+    return handler(state, action)    
+    
 
     
 }
